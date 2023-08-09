ce48dc95f70a53997ad31a43fc8922d33eb7ba94
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-08-09 13:12:26 +0300

    feature: add 3 endpoints to communicate with entity, link avatars with students

---
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -0,0 +1,57 @@
+package ru.hogwarts.school.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.hibernate.result.Output;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.service.AvatarService;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@RequestMapping("/avatar")
+public class AvatarController {
+    private final AvatarService avatarService;
+
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadStudentAvatar(@PathVariable long id,
+                                                      @RequestParam MultipartFile avatar) throws IOException {
+        avatarService.uploadStudentAvatar(id, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}/preview")
+    public ResponseEntity<byte[]> getStudentAvatarPreview(@PathVariable long id) {
+        Avatar avatar = avatarService.findAvatarOrThrow(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getPreview().length);
+        return ResponseEntity.ok().headers(headers).body(avatar.getPreview());
+    }
+
+    @GetMapping("/{id}/real")
+    public void getStudentAvatarInRealSize(@PathVariable long id,
+                                           HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findAvatarOrThrow(id);
+        try (
+                InputStream is = Files.newInputStream(Path.of(avatar.getFilePath()));
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream())
+        ) {
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength(avatar.getFileSize().intValue());
+            response.setStatus(HttpServletResponse.SC_OK);
+            bis.transferTo(bos);
+        }
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -0,0 +1,101 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.exception.AvatarNotFoundException;
+import ru.hogwarts.school.exception.FileIsTooBigException;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+
+@Service
+@Transactional
+public class AvatarServiceImpl implements AvatarService {
+
+    @Value("${students.avatars.dir.path}")
+    private String avatarsDir;
+    private final AvatarRepository avatarRepository;
+    private final StudentService studentService;
+
+    public AvatarServiceImpl(AvatarRepository avatarRepository, StudentService studentService) {
+        this.avatarRepository = avatarRepository;
+        this.studentService = studentService;
+    }
+
+    @Override
+    public void uploadStudentAvatar(long studentId, MultipartFile avatarFile) throws IOException {
+        if (avatarFile.getSize() > (1024 * 300)) {
+            throw new FileIsTooBigException();
+        }
+
+        Student student = studentService.getStudent(studentId);
+        Path filePath = Path.of(avatarsDir, studentId + "." +
+                getExtension(avatarFile.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (
+                InputStream is = avatarFile.getInputStream();
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+        Avatar avatar = findAvatar(studentId);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setPreview(generateImagePreview(filePath));
+        avatar.setStudent(student);
+        avatarRepository.save(avatar);
+    }
+
+    @Override
+    public Avatar findAvatar(long studentId) {
+        Avatar avatar = avatarRepository.findByStudentId(studentId);
+        return avatar == null ? new Avatar() : avatar;
+    }
+
+    @Override
+    public Avatar findAvatarOrThrow(long studentId) {
+        Avatar avatar = avatarRepository.findByStudentId(studentId);
+        if (avatar == null) {
+            throw new AvatarNotFoundException();
+        }
+        return avatar;
+    }
+
+    private byte[] generateImagePreview(Path filePath) throws IOException {
+        try (
+                InputStream is = Files.newInputStream(filePath);
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream()
+            ) {
+            BufferedImage image = ImageIO.read(bis);
+
+            int height = image.getHeight() / (image.getWidth() / 100);
+            BufferedImage preview = new BufferedImage(100, height, image.getType());
+            Graphics2D graphics = preview.createGraphics();
+            graphics.drawImage(image, 0, 0, 100, height, null);
+            graphics.dispose();
+
+            ImageIO.write(preview, getExtension(filePath.getFileName().toString()), baos);
+            return baos.toByteArray();
+        }
+    }
+
+    private String getExtension(String filename) {
+        return filename.substring(filename.lastIndexOf(".") + 1);
+    }
+}
