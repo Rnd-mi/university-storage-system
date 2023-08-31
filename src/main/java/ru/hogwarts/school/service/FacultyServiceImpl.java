d829d061f4a434e5107432afe7cc62c3d1b14799
Ivan Stepanov <SireOfAll79@yandex.ru>
2023-08-31 14:52:19 +0300

    feature: add logging for 'FacultyService' class

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.FacultyAlreadyExistsException;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
@@ -12,12 +14,15 @@ import java.util.Collection;
 import java.util.List;
 
 import static ru.hogwarts.school.utility.InputValidator.validateFacultyProps;
+import static ru.hogwarts.school.utility.MessageGenerator.*;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
     private final FacultyRepository facultyRepository;
 
     private final StudentRepository studentRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
 
     public FacultyServiceImpl(FacultyRepository facultyRepository,
                               StudentRepository studentRepository) {
@@ -27,28 +32,33 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty createFaculty(Faculty faculty) {
+        logger.info(generateMsgIfMethodInvoked("createFaculty"));
         validateFacultyProps(faculty);
         try {
             return facultyRepository.save(faculty);
         } catch (Exception e) {
+            logger.error(generateMsgWhenException(getAlreadyExistsException()));
             throw new FacultyAlreadyExistsException();
         }
     }
 
     @Override
     public Faculty getFaculty(long id) {
+        logger.info(generateMsgIfMethodInvoked("getFaculty"));
         checkIfExist(id);
         return facultyRepository.findById(id).get();
     }
 
     @Override
     public Faculty updateFaculty(Faculty faculty) {
+        logger.info(generateMsgIfMethodInvoked("updateFaculty"));
         checkIfExist(faculty.getId());
         return createFaculty(faculty);
     }
 
     @Override
     public void deleteFaculty(long id) {
+        logger.info(generateMsgIfMethodInvoked("deleteFaculty"));
         checkIfExist(id);
         List<Student> students = studentRepository.findStudentsByFacultyId(id);
 
@@ -61,9 +71,11 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> getFacultiesOfColor(String color) {
+        logger.info(generateMsgIfMethodInvoked("getFacultiesOfColor"));
         Collection<Faculty> faculties = facultyRepository.findByColorIgnoreCase(color);
 
         if (faculties.isEmpty()) {
+            logger.error(generateMsgWhenException(getNotFoundException()));
             throw new FacultyNotFoundException();
         }
         return faculties;
@@ -71,9 +83,11 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> getAll() {
+        logger.info(generateMsgIfMethodInvoked("getAll"));
         Collection<Faculty> faculties = facultyRepository.findAll();
 
         if (faculties.isEmpty()) {
+            logger.error(generateMsgWhenException(getNotFoundException()));
             throw new FacultyNotFoundException();
         }
         return faculties;
@@ -81,9 +95,11 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> getFacultyByColorOrName(String color, String name) {
+        logger.info(generateMsgIfMethodInvoked("getFacultyByColorOrName"));
         Collection<Faculty> result = facultyRepository.findByColorIgnoreCaseOrNameIgnoreCase(color, name);
 
         if (result.isEmpty()) {
+            logger.error(generateMsgWhenException(getNotFoundException()));
             throw new FacultyNotFoundException();
         }
         return result;
@@ -91,14 +107,25 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Student> getStudents(long id) {
+        logger.info(generateMsgWhenException("getStudents"));
         checkIfExist(id);
         Faculty faculty = facultyRepository.findById(id).get();
         return faculty.getStudents();
     }
 
     public void checkIfExist(long id) {
+        logger.debug(generateMsgIfMethodInvoked("checkIfExist"));
         if (!facultyRepository.existsById(id)) {
+            logger.error(generateMsgWhenException(getNotFoundException(), id));
             throw new FacultyNotFoundException();
         }
+    }
+
+    private String getNotFoundException() {
+        return "FacultyNotFoundException";
+    }
+
+    private String getAlreadyExistsException() {
+        return "FacultyAlreadyExistsException";
     }
 }
